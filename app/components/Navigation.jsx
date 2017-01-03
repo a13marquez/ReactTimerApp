e8066a7d4448782198f7af07571c3ba05a25254b
Álvaro Márquez <marquezalvaro13@gmail.com>
2017-01-03 12:15:35 +0100

    first commit

---
+++ app/components/Navigation.jsx
@@ -0,0 +1,28 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Navigation = () => {
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Time App</li>
+            <li>
+              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Timer</IndexLink>
+            </li>
+            <li>
+              <Link to="/countdown" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Countdown</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <ul className="menu">
+            <li className="menu-text">Created by <a href="https://github.com/a13marquez" target="_blank ">Álvaro Márquez</a></li> 
+          </ul>
+        </div>
+      </div>
+    );
+  }
+
+
+module.exports = Navigation;
